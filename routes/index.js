00a71fa10a566881f84e868a43a51e73a4448e3b
gourranz <Zakaria.Gourrane@lions.enc.edu>
2023-12-30 21:34:46 -0500

    Login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 router.get('/', function(req, res, next) {
   res.redirect('/teams/dashboard');
+});
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    prompt: "select_account"
+  }
+));
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/teams',
+    failureRedirect: '/teams'
+  }
+));
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/teams');
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
 require('dotenv').config();
 // connect to the database with AFTER the config vars are processed
 require('./config/database');
+require('./config/passport');
 
 //server.js
 const indexRouter = require('./routes/index');
@@ -28,6 +31,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/teams', teamRouter);
