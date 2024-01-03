d455a3de9bca7e62af6655681ae3326ea3ced180
gourranz <Zakaria.Gourrane@lions.enc.edu>
2024-01-03 00:27:58 -0500

    Player

--- models/player.js
+++ models/player.js
@@ -1,12 +1,23 @@
+//models/player.js
 const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
   playerName: { type: String, required: true },
   position: { type: String, required: true },
-  team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true },
-  // Add other fields as needed
+  team: { type: String, required: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  id: String,
+  userName: String,
+  userAvatar: String
+}, {
+  timestamps: true
 });
+  
 
 const Player = mongoose.model('Player', playerSchema);
 
 module.exports = Player;
+
