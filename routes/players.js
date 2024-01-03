39fddb5417f98a44810c4572113943201d6d44f1
gourranz <Zakaria.Gourrane@lions.enc.edu>
2024-01-03 00:33:32 -0500

    team

--- routes/players.js
+++ routes/players.js
@@ -3,11 +3,21 @@
 const express = require('express');
 const router = express.Router();
 const playerController = require('../controllers/player');
+const User = require('../models/user');
 
 router.get('/dashboard', playerController.renderDashboard);
 router.get('/', playerController.renderPlayers);
 router.get('/add', playerController.renderAddPlayerForm);
 router.post('/', playerController.addPlayer);
+router.get('/users', async (req, res, next) => {
+    try {
+      const users = await User.find().populate('favoriteTeams addedPlayers');
+      res.render('users', { title: 'Users', users });
+    } catch (error) {
+      console.error(error);
+      res.render('error', { error });
+    }
+  });
 
 module.exports = router;
 
