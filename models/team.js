2fe0fe5a16819e60544e019f5bf996cb6744fc69
gourranz <Zakaria.Gourrane@lions.enc.edu>
2024-01-03 00:28:32 -0500

    team

--- models/team.js
+++ models/team.js
@@ -4,6 +4,9 @@ const teamSchema = new mongoose.Schema({
   teamName: { type: String, required: true },
   country: { type: String, required: true },
   coach: { type: String, required: true },
+  addedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  userName: String,
+  userAvatar: String
 });
 
 const Team = mongoose.model('Team', teamSchema);
